298cc276789a57f432742471c3c6208a24b47dbb
seang673 <seangriffiths673@gmail.com>
2026-04-16 01:22:41 -0400

    Backend Implementation

---
+++ backend/app/db/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+
+# Pydantic models for job postings. Represents how data is validated and returned
+class JobPostingBase(BaseModel):
+    title: str
+    company: str
+    location: Optional[str] = None
+    description: Optional[str] = None
+    url: Optional[str] = None
+    source: Optional[str] = "indeed"
+    date_posted: Optional[str] = None
+
+class JobPostingCreate(JobPostingBase):
+    pass
+
+class JobPosting(JobPostingBase):
+    id: int
+    date_scraped: Optional[str] = None
+    skills_extracted: Optional[str] = None
+    summary: Optional[str] = None
+    embedding_id: Optional[str] = None
+
+    class Config:
+        from_attributes = True   #Tells Pydantic to read data from ORM models using attribute access
